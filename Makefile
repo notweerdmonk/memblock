bf77a153fbbbd00e9b156689c97420e2c819b3eb
notweerdmonk <wrdmnk@gmail.com>
2022-06-24 12:01:34 +0530

    Add another test for structs.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 all: test
 
 test: memblock.h test.c
-	gcc -Wall -ggdb -o test test.c
+	gcc -Wall -ggdb -I. -o test test.c
 
 exp:
 	gcc -fno-stack-protector -o exploit exploit.c
--- test.c
+++ test.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "memblock.h"
+#include <memblock.h>
 
-int main() {
+void test1() {
   char array[42];
 
   struct memblock int_memblock;
@@ -30,6 +31,8 @@ int main() {
   *x = 108;
   *y = 109;
 
+  printf("test 1\n");
+
   printf("Data in memory locations:\n");
   printf("%d\n", ((int*)array)[0]);
   printf("%d\n", ((int*)array)[1]);
@@ -44,11 +47,11 @@ int main() {
 
   int *z = (int*)use_mem(int, int_memblock);
   if (z == NULL) {
-    printf("Memblock exhausted!\n");
+    printf("Memblock exhausted\n");
   }
 
   free_mem(r, int_memblock);
-  free_mem(u, int_memblock);
+  free_mem(s, int_memblock);
   free_mem(x, int_memblock);
 
   int *a = use_mem(int, int_memblock);
@@ -62,8 +65,71 @@ int main() {
 
   printf("Data after reusing memory locations:\n");
   printf("%d\n", ((int*)array)[8]);
-  printf("%d\n", ((int*)array)[5]);
+  printf("%d\n", ((int*)array)[3]);
   printf("%d\n", ((int*)array)[2]);
+}
+
+/* struct of size 16 bytes with padding */
+struct foo {
+  int n;
+  double f;
+};
+
+void test2() {
+  int *array = (int*)malloc(64 * sizeof(int));
+
+  struct memblock foo_memblock;
+  init_mem(foo_memblock, struct foo, array, 4);
+
+  struct foo *p = use_mem(struct foo, foo_memblock);
+  struct foo *q = use_mem(struct foo, foo_memblock);
+  struct foo *r = use_mem(struct foo, foo_memblock);
+  struct foo *s = use_mem(struct foo, foo_memblock);
+
+  p->n = 101;
+  p->f = 100.1;
+  q->n = 102;
+  q->f = 100.2;
+  r->n = 103;
+  r->f = 100.3;
+  s->n = 104;
+  s->f = 100.4;
+
+  printf("test 2\n");
+
+  printf("Data in memory locations:\n");
+  printf("%d %f\n", ((struct foo*)array)[0].n, ((struct foo*)array)[0].f);
+  printf("%d %f\n", ((struct foo*)array)[1].n, ((struct foo*)array)[1].f);
+  printf("%d %f\n", ((struct foo*)array)[2].n, ((struct foo*)array)[2].f);
+  printf("%d %f\n", ((struct foo*)array)[3].n, ((struct foo*)array)[3].f);
+  printf("%d %f\n", ((struct foo*)array)[4].n, ((struct foo*)array)[4].f);
+
+  struct foo *t = use_mem(struct foo, foo_memblock);
+  if (t == NULL) {
+    printf("Memblock exhausted\n");
+  }
+
+  free_mem(p, foo_memblock);
+  free_mem(r, foo_memblock);
+
+  struct foo *a = use_mem(struct foo, foo_memblock);
+  a->n = 1001;
+  a->f = 1000.1;
+
+  struct foo *b = use_mem(struct foo, foo_memblock);
+  b->n = 9999;
+  b->f = 9999.9;
+
+  printf("Data after reusing memory locations:\n");
+  printf("%d %f\n", ((struct foo*)array)[2].n, ((struct foo*)array)[2].f);
+  printf("%d %f\n", ((struct foo*)array)[0].n, ((struct foo*)array)[0].f);
+
+  free(array);
+}
+
+int main() {
+  test1();
+  test2();
 
   return 0;
 }
